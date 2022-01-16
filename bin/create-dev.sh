c76333640defe4fd23c62a4bd5cf04a64245937e
Dan Munro <dan@danmunro.com>
2022-01-16 08:54:06 -0800

    fix

--- bin/create-dev.sh
+++ bin/create-dev.sh
@@ -8,7 +8,8 @@ kubectl apply -f apps/dev/postgres.yaml
 
 kubectl apply -f secrets/dev/dockerconfigjson.yaml
 kubectl apply -f secrets/dev/kafka.yaml
-kubectl apply -f secrets/dev/postgres.yaml
+kubectl apply -f secrets/dev/postgres-community.yaml
+kubectl apply -f secrets/dev/postgres-user.yaml
 kubectl apply -f secrets/dev/cognito.yaml
 kubectl apply -f secrets/dev/aws.yaml
 kubectl apply -f secrets/dev/image_service.yaml
