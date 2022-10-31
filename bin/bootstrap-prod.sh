4c83619df377ec8fa0b8c8227c058e17ccb5d3f9
Dan Munro <dan@danmunro.com>
2022-10-30 18:41:19 -0700

    add secrets

--- bin/bootstrap-prod.sh
+++ bin/bootstrap-prod.sh
@@ -1,6 +1,13 @@
 set -e
 
 kubectl apply -f secrets/prod/dockerconfigjson.yaml
+kubectl apply -f secrets/prod/kafka.yaml
+kubectl apply -f secrets/prod/postgres-community.yaml
+kubectl apply -f secrets/prod/postgres-user.yaml
+kubectl apply -f secrets/prod/postgres-image.yaml
+kubectl apply -f secrets/prod/cognito.yaml
+kubectl apply -f secrets/prod/aws.yaml
+kubectl apply -f secrets/prod/image_service.yaml
 
 flux check --pre
 
