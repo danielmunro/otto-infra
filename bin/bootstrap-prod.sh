e20f9fa0ed34d0f936f9fc0de7dd47411e17c69f
Dan Munro <dan@danmunro.com>
2022-11-01 07:25:35 -0700

    add missed steps

--- bin/bootstrap-prod.sh
+++ bin/bootstrap-prod.sh
@@ -9,6 +9,14 @@ kubectl apply -f secrets/prod/cognito.yaml
 kubectl apply -f secrets/prod/aws.yaml
 kubectl apply -f secrets/prod/image_service.yaml
 
+helm install ingress-nginx ingress-nginx/ingress-nginx
+
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --set global.leaderElection.namespace=cert-manager
+
 flux check --pre
 
 flux bootstrap github \
